5a1ebd8a78ffc185c3246634fb24b90866d6e517
minhnguyen957 <148942661+minhnguyen957@users.noreply.github.com>
2024-05-27 01:51:03 +0700

    update 27/5/2024

--- HTTSWebApplication/target/generated-sources/annotations/com/lnht/pojo/Banner_.java
+++ HTTSWebApplication/target/generated-sources/annotations/com/lnht/pojo/Banner_.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-05-26T18:10:46", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-05-26T18:55:10", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Banner.class)
 public class Banner_ { 
 
--- HTTSWebApplication/target/generated-sources/annotations/com/lnht/pojo/Loaituyensinh_.java
+++ HTTSWebApplication/target/generated-sources/annotations/com/lnht/pojo/Loaituyensinh_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-05-26T18:10:46", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-05-26T18:55:10", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Loaituyensinh.class)
 public class Loaituyensinh_ { 
 
